388f1ed42c7a0bf9c8f4f97abaf5bc3a0bec48c7
martinJCH20 <61629740+martinJCH20@users.noreply.github.com>
2020-04-18 10:52:44 -0500

    Add files via upload

---
+++ ejercicio8.js
@@ -0,0 +1,24 @@
+function elevate(number){
+    let cuadrado = Math.pow(number,2);
+    let cubo = Math.pow(number,3);
+    return `N° ${number} - cuadrado: ${cuadrado} - cubo: ${cubo}`;
+}
+
+function doMath(valores, callFunction){
+    var newNumber = [];
+    if(typeof callFunction === 'function'){
+        valores.map((element) => {
+            if(typeof element === 'number'){
+                newNumber.push(callFunction(element));
+            }else{
+                newNumber.push(`El valor ${element} no es un número`);
+            }
+        });
+        console.log(newNumber);
+    }else{
+        console.log(`Debe ingresar la función`);
+    }
+}
+
+let numbers = [2,'1',7,32]
+doMath(numbers, elevate);
