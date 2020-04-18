6e70e6d0002ae7b67f2a1fe6b379e06d132ce75a
martinJCH20 <61629740+martinJCH20@users.noreply.github.com>
2020-04-18 08:43:35 -0500

    Add files via upload

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,9 +1,28 @@
 let paises = [
-    'Perú','Australia','Dinamarca','España'
+    {
+        name: 'Perú',
+        habitantes: 35000000
+    },
+    {
+        name: 'Australia',
+        habitantes: 10000000
+    }
+    ,
+    {
+        name: 'Dinamarca',
+        habitantes: 4000000
+    }
 ]
 
-const taskPaices = paises.map((task) => {
-    return task;
-});
+const mapPaices = function (array, callback){
+    let newArray = [];
+        array.forEach(function(element, index, array){
+        newArray.push(callback(element));
+    })
+    return newArray;
+};
 
-console.log(taskPaices);
+const paisesNombres = mapPaices(paises, function(task){
+    return task.name;
+})
+console.log(paisesNombres);
